743fc67a3a4f4870364ab933b3a2609cc5169e1e
GriffinAustin <gaustin25@gmail.com>
2019-03-01 11:39:41 -0700

    Add support for 7-day forecast as well as geolocation

--- app.py
+++ app.py
@@ -4,31 +4,41 @@ import geocoder
 
 app = Flask(__name__)
 
+# Get geolocation of user
 geoloc = geocoder.ip('me')
 lat = geoloc.latlng[0]
 lng = geoloc.latlng[1]
 
+# Define API
 api_endpoint = 'https://api.weather.gov'
 api_uri = api_endpoint + '/points/{},{}'.format(str(lat), str(lng))
-
+r = requests.get(api_uri)
+loc = r.json()['properties']['relativeLocation']['properties']['city'] + ', ' + r.json()['properties']['relativeLocation']['properties']['state']
 
 def get_forecast():
+    # Retrieve uri
     r = requests.get(api_uri)
     forecast_uri = r.json()['properties']['forecast']
     r = requests.get(forecast_uri)
-    today = r.json()['properties']['periods'][0]
-    data = {
-            'name': today['name'],
-            'temperature': today['temperature'],
-            'wind': today['windSpeed'] + ' ' + today['windDirection']
-            }
+
+    # Loop through forecast data
+    properties = r.json()['properties']
+    periods = r.json()['properties']['periods']
+    num_of_periods = len(periods)
+    data = {}
+    for i in range(num_of_periods):
+        sub_data = {
+                    'name': periods[i]['name'],
+                    'temperature': periods[i]['temperature']
+                    }
+        data.update({str(i): sub_data})
     return data
 
 
 @app.route("/")
 def main():
     data = get_forecast()
-    return render_template('base.html', lat=lat, lng=lng, data=data)
+    return render_template('base.html', loc=loc, lat=lat, lng=lng, data=data)
 
 
 if __name__ == "__main__":
